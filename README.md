614096fb154527980195737daaf2b8a5f5bfa794
Armaan Gupta <armaangupta@MacBook-Air.local>
2024-09-15 15:59:34 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+//Add to this# armaans-world
---
+++ src/assets/index.js
@@ -0,0 +1,64 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import python from "./tech/python.png";
+import java from "./tech/java.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import c from "./tech/C.png";
+import racket from "./tech/racket.svg";
+import reactjs from "./tech/reactjs.png";
+import tensorflow from "./tech/tensorflow.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+import pytorch from "./tech/pytorch.png";
+
+import code_ninjas from "./company/code-ninjas.png";
+import landingX from "./company/landingx.png";
+import tks from "./company/tks.jpg";
+import hippocampus from "./company/hippocampus.jpg";
+
+import website from "./website.png";
+import mnist from "./MNIST.png";
+import aimtrainer from "./aimtrainer.png";
+import article from "./medium_article.png"; 
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  python,
+  java,
+  git,
+  html,
+  javascript,
+  c,
+  racket,
+  reactjs,
+  tensorflow,
+  tailwind,
+  typescript,
+  pytorch,
+  code_ninjas,
+  landingX,
+  tks,
+  hippocampus,
+  website,
+  mnist,
+  aimtrainer,
+  article,
+};
